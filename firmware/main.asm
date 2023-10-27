1b88e3c250611e240620c18ac26e8e70014053a2
Mike Naberezny <mike@naberezny.com>
2023-10-27 09:06:55 -0700

    Clean up the GPIO routines a bit

--- firmware/main.asm
+++ firmware/main.asm
@@ -4,7 +4,7 @@
 ;3  PA5 out SLOUT 
 ;4  PA6 out CAPSOUT 
 ;5  PA7 out 4080OUT 
-;6  PB3 NC
+;6  PB3 out /CBMRESET (0=/CBMRESET=high, 1=/CBMRESET=low)
 ;7  PB2 in 4080KEY (0=down, 1=up)
 ;8  PB1 in CAPSKEY 
 ;9  PB0 in SLKEY 
@@ -13,19 +13,6 @@
 ;12 PA2 out CAPSLED
 ;13 PA3 out 4080LED 
 ;14 GND
-
-;shift_lock  SLKEY PB0 = 1
-;            SLOUT PA5 = 32
-;            SLLED PA1 = 2
-;
-;caps_lock   CAPSKEY PB1 = 2
-;            CAPSOUT PA6 = 64
-;            CAPSLED PA2 = 4
-;
-;forty_lock  4080KEY PB2 = 4
-;            4080OUT PA7 = 64
-;            4080LED PA3 = 8
-
 
     ;Definitions file "tn214def.asm"
     ;will be included first by the Makefile.
@@ -69,71 +56,68 @@ reset:
     rcall wdog_init
     rcall gpio_init
 
+    ldi r19, 0
+
 main_loop:
     wdr                       ;Keep watchdog happy
-    rcall read_keys
-    rcall set_leds
-    rcall set_contacts
+    rcall gpio_read_keys
+    eor r19, r16
+    mov r16, r19
+    rcall gpio_set_leds
+    rcall gpio_set_contacts
     rjmp main_loop
 
-;Set up GPIO directions
-gpio_init:
-    ;Button Inputs
-    ldi r16, 1<<2 | 1<<1 | 1<<0     ;PB2, PB1, PB0
-    sts PORTB_DIRCLR, r16           ;set pins as input
-
-    ;4066 Outputs
-    ldi r16, 1<<7 | 1<<6 | 1<<5 | 1<<4  ;PA7, PA6, PA5, PA4
-    sts PORTA_OUTCLR, r16               ;set 4066s initially off (0=off)
-    sts PORTA_DIRSET, r16               ;set pins as outputs
-
-    ;LED Outputs
-    ldi r16, 1<<3 | 1<<2 | 1<<1     ;PA3, PA2, PA1
-    sts PORTA_OUTSET, r16           ;sets LEDs initially off (1=off)
-    sts PORTA_DIRSET, r16           ;set pins as outputs
+;Wait 1ms.  Destroy R16,R17
+delay_1ms:
+    ldi r16, 6
+1$: ldi r17, 0xc5
+2$: dec r17
+    brne 2$
+    dec r16
+    brne 1$
     ret
 
+;GPIO =======================================================================
 
-;Returns key status in R16:
 ;
-;   Bit 2 = 4080 key (0=up, 1=down)
-;   Bit 1 = caps key
-;   Bit 0 = shift lock key
+;The GPIO routines abstract the I/O pins such that the same bit
+;pattern is used for the key inputs, LED outputs, and 4066 outputs.
+;The bit values use positive logic:
 ;
-read_keys:
-    ldi r17, 1<<2 | 1<<1 | 1<<0
+; - A bit of "0" means key up / LED off / 4066 contact open
+; - A bit of "1" means key down / LED on / 4066 contact closed
+;
+
+;Read the key switch inputs and return them in R16
+;0=key up, 1=key down
+;
+;R16:
+;  Bit 3 = Always 0 (would be Extra key)
+;  Bit 2 = 40/80 key 
+;  Bit 1 = Caps Lock key
+;  Bit 0 = Shift Lock key
+;
+gpio_read_keys:
+    ;Read PORTB_IN and convert to R16 bits
     lds r16, PORTB_IN
-    eor r16, r17
-    ret
-
-;Sets LEDs from R16
-;
-;   Bit 2 = 4080 LED (0=off, 1=on)
-;   Bit 1 = caps lock LED
-;   Bit 0 = shift lock LED
-;
-set_leds:
-    push r16
-    lsl r16
-    ldi r17, 1<<3 | 1<<2 | 1<<1
+    ldi r17, 1<<2 | 1<<1 | 1<<0
     and r16, r17
-    eor r16, r17
-
-    lds r17, PORTA_OUT
-    andi r17, 0xff ^ (1<<3 | 1<<2 | 1<<1)    
-    or r17, r16
-    sts PORTA_OUT, r17
-    pop r16
+    eor r16, r17  ;PORTB_IN bits are inverted (0=key down)
     ret
 
-;Sets 4066 contacts from R16
+;Set the 4066 contacts from the value in R16
+;0=contact open, 1=contact closed
 ;
-;   Bit 2 = 4080 contact (0=off, 1=on)
-;   Bit 1 = caps lock contact
-;   Bit 0 = shift lock contact
+;R16:
+;  Bit 3 = Extra contact
+;  Bit 2 = 40/80 contact
+;  Bit 1 = Caps Lock contact
+;  Bit 0 = Shift Lock contact
 ;
-set_contacts:
+gpio_set_contacts:
     push r16
+
+    ;Convert R16 bits to PORTA_OUT bits
     lsl r16 
     lsl r16 
     lsl r16 
@@ -141,13 +125,70 @@ set_contacts:
     lsl r16 
     andi r16, 1<<7 | 1<<6 | 1<<5 | 1<<4
 
+    ;Set/clear bits in PORTA_OUT
     lds r17, PORTA_OUT
     andi r17, 0xff ^ (1<<7 | 1<<6 | 1<<5 | 1<<4)
     or r17, r16
     sts PORTA_OUT, r17
+
     pop r16
     ret
 
+;Set the LEDs from the value in R16
+;0=LED off, 1=LED on
+;
+;R16:
+;  Bit 3 = Ignored (would be Extra LED)
+;  Bit 2 = 40/80 LED
+;  Bit 1 = Caps Lock LED
+;  Bit 0 = Shift Lock LED
+;
+gpio_set_leds:
+    push r16
+
+    ;Convert R16 bits to PORTA_OUT bits
+    lsl r16
+    ldi r17, 1<<3 | 1<<2 | 1<<1
+    and r16, r17
+    eor r16, r17  ;PORTA_OUT bits are inverted (0=LED on)
+
+    ;Set/clear bits in PORTA_OUT
+    lds r17, PORTA_OUT
+    andi r17, 0xff ^ (1<<3 | 1<<2 | 1<<1)    
+    or r17, r16
+    sts PORTA_OUT, r17
+
+    pop r16
+    ret
+
+;Set initial GPIO directions and states
+;
+; - LED pins as outputs; LEDs not lit
+; - 4066 pins as outputs; contacts open
+; - /CBMRESET pint as output; /CBMRESET=high
+;
+gpio_init:
+    ;Key Inputs
+    ldi r16, 1<<2 | 1<<1 | 1<<0     ;PB2, PB1, PB0
+    sts PORTB_DIRCLR, r16           ;Set pins as input
+
+    ;4066 Outputs
+    ldi r16, 1<<7 | 1<<6 | 1<<5 | 1<<4  ;PA7, PA6, PA5, PA4
+    sts PORTA_OUTCLR, r16           ;Set 4066s initially off (0=off)
+    sts PORTA_DIRSET, r16           ;Set pins as outputs
+
+    ;LED Outputs
+    ldi r16, 1<<3 | 1<<2 | 1<<1     ;PA3, PA2, PA1
+    sts PORTA_OUTSET, r16           ;Sets LEDs initially off (1=off)
+    sts PORTA_DIRSET, r16           ;Set pins as outputs
+
+    ;/CBMRESET Output
+    ldi r16, 1<<3                   ;PB3
+    sts PORTA_OUTCLR, r16           ;Set /CBMRESET initially high (0=high)
+    sts PORTB_DIRSET, r16           ;Set PB3 as output
+    ret
+
+;WATCHDOG ===================================================================
 
 ;Ensure the watchdog was started by the fuses and reset the timer.
 ;The WDR instruction must be executed at least once every 4 seconds
@@ -168,17 +209,7 @@ wdog_init:
     wdr                               ;Reset watchdog timer
     ret
 
-;Wait 1ms.  Destroy R16,R17
-delay_1ms:
-    ldi r16, 6
-1$: ldi r17, 0xc5
-2$: dec r17
-    brne 2$
-    dec r16
-    brne 1$
-    ret
-
-;End of code
+;END OF CODE ================================================================
 
     ;Fill all unused program words with a nop sled that ends with
     ;a software reset in case the program counter somehow gets here.
