17855f759a7f417d9272010fdebdcfe8245da95e
Mike Naberezny <mike@naberezny.com>
2023-11-01 13:35:59 -0700

    Run with 20 MHz internal oscillator

--- firmware/fuses.asm
+++ firmware/fuses.asm
@@ -14,7 +14,7 @@
 
 ;Oscillator Configuration
 .org 0+FUSE_OSCCFG_offset
-.byte (0<<FUSE_OSCLOCK_bp) | FUSE_FREQSEL_16MHZ_gc
+.byte (0<<FUSE_OSCLOCK_bp) | FUSE_FREQSEL_20MHZ_gc
 
 ;Reserved fuse between OSCCFG and TCD0CFG should not be programmed
 
